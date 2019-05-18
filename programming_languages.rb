83835a23dd9b478aa773fa452309831a10f4b836
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-18 06:09:18 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
 def reformat_languages(languages)
-  # your code here
+  reformat_languages = {}
+  languages.each { |style, all_language|
+    all_language.each {|language, char| 
+      char.each {|type, type_answer|
+        reformat_languages[language] = {
+          type: type_answer
+          style: []
+        }
+      }
+    }
+  }
+  languages.each {|style, all_language|
+    all_language.each {|language, char|
+      reformat_languages[language][:style].push(style)
+    }
+  }
+  reformat_languages
 end
